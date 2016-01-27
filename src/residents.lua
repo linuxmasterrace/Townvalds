189a172e396b7253f7c0ab5dd96ebcfa5eb36ee6
random-nick <random-nick@mail.com>
2016-01-27 22:14:22 +0100

    Implement a very basic and unfinished residents system

---
+++ src/residents.lua
@@ -0,0 +1,35 @@
+function DisplayResident(Split, Player)
+   -- Check if the player has entered the name of the resident
+   if (Split[2] == nil) then
+	  Player:SendMessageFailure("You have to enter a resident name! Usage: /resident (username)")
+	  return true
+   end
+
+   -- Retrieve the player UUID of the resident from Mojang
+   --   local UUID = cMojangAPI:GetUUIDFromPlayerName(Split[2], true)
+   --
+   -- if (UUID == "") then
+   --  Player:SendMessageFailure("Invalid player!")
+   --return true
+   -- end
+
+   -- Query the resident info from the database
+   local sql = "SELECT residents.player_name, towns.town_name, residents.town_rank, residents.last_online FROM residents LEFT JOIN towns ON towns.town_id = residents.town_id WHERE residents.player_name = ?"
+   local parameters = {Player:GetName()}
+   local result = ExecuteStatement(sql, parameters)
+
+   if(result==nil) then
+	  Player:SendMessageFailure("Player not found in database!!")
+   else
+	  if(result[1]==nil) then
+		 Player:SendMessageFailure("Player not found in database!")
+	  else
+		 Player:SendMessageInfo("Username: "..(result[1][1] or "none"))
+		 Player:SendMessageInfo("Town: "..(result[1][2] or "none"))
+		 Player:SendMessageInfo("Town Rank: "..(result[1][3] or "none"))
+		 Player:SendMessageInfo("Last Online: "..(result[1][4] or "none"))
+	  end
+   end
+
+   return true
+end
