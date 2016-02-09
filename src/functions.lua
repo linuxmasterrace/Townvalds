f7a1f1bae2d2cd86705ea8579f7171d070f59593
BuildTools <bart.ribbers@gmail.com>
2016-02-09 16:05:17 +0100

    Added basic town protection

--- src/functions.lua
+++ src/functions.lua
@@ -1,53 +1,79 @@
 InTown = {}
 
 function CheckPlayerInTown(Player, chunkX, chunkZ)
-   local sql = "SELECT towns.town_name, townChunks.chunkX, townChunks.chunkZ FROM townChunks LEFT JOIN towns ON towns.town_id = townChunks.town_id WHERE townChunks.chunkX = ? AND townChunks.chunkZ = ?";
-   local parameters = {chunkX, chunkZ};
-   local result = ExecuteStatement(sql, parameters);
-   local town = InTown[Player:GetName()];
-   if(result[1] and result[1][1] and result[1][1] ~= town) then
-      Player:SendMessage("You're in the town " .. result[1][1]);
-      InTown[Player:GetName()] = result[1][1];
-   elseif (not(result[1] and result[1][1]) and town) then
-      Player:SendMessage("You have left the town "..town..".");
-      InTown[Player:GetName()] = nil;
-   end
+    local sql = "SELECT towns.town_name, townChunks.chunkX, townChunks.chunkZ FROM townChunks LEFT JOIN towns ON towns.town_id = townChunks.town_id WHERE townChunks.chunkX = ? AND townChunks.chunkZ = ?";
+    local parameters = {chunkX, chunkZ};
+    local result = ExecuteStatement(sql, parameters);
+    local town = InTown[Player:GetName()];
+    if(result[1] and result[1][1] and result[1][1] ~= town) then
+        Player:SendMessage("You're in the town " .. result[1][1]);
+        InTown[Player:GetName()] = result[1][1];
+    elseif (not(result[1] and result[1][1]) and town) then
+        Player:SendMessage("You have left the town "..town..".");
+        InTown[Player:GetName()] = nil;
+    end
+end
+
+function CheckBlockPermission(Player, BlockX, BlockZ)
+    local sql = "SELECT town_id FROM townChunks WHERE townChunks.chunkX = ? AND townChunks.chunkZ = ?";
+    local parameters = {math.floor(BlockX / 16), math.floor(BlockZ / 16)};
+    local town_id = ExecuteStatement(sql, parameters);
+    if (town_id[1] and town_id[1][1]) then --The block being broken is part of a town
+        local sql = "SELECT * FROM residents WHERE player_uuid = ? AND town_id = ?";
+        local parameters = {cMojangAPI:GetUUIDFromPlayerName(Player:GetName(), true), town_id[1][1]};
+        local result = ExecuteStatement(sql, parameters);
+        if (result[1] and result[1][1]) then -- Player is in a town he belongs to
+            return false;
+        else -- Player is in a town he doesn't belong to, prevent block breaking
+            return true;
+        end
+    else -- The block being broken is NOT part of a town
+        return false;
+    end
 end
 
 function OnPlayerJoined(Player)
-   local sql = "INSERT OR IGNORE INTO residents (player_uuid, player_name, town_id, town_rank, last_online) VALUES (?, ?, NULL, NULL, datetime(\"now\"))";
-   local parameters = {cMojangAPI:GetUUIDFromPlayerName(Player:GetName(), true), Player:GetName()};
-   if(ExecuteStatement(sql, parameters)==nil) then
-      LOG("Couldn't add player "..Player:GetName().." to the database!!!")
-   end
+    local sql = "INSERT OR IGNORE INTO residents (player_uuid, player_name, town_id, town_rank, last_online) VALUES (?, ?, NULL, NULL, datetime(\"now\"))";
+    local parameters = {cMojangAPI:GetUUIDFromPlayerName(Player:GetName(), true), Player:GetName()};
+    if(ExecuteStatement(sql, parameters) == nil) then
+        LOG("Couldn't add player "..Player:GetName().." to the database!!!");
+    end
 end
 
 function OnPlayerSpawned(Player) -- This is called after both connection and respawning
-   CheckPlayerInTown(Player, Player:GetChunkX(), Player:GetChunkZ());
+    CheckPlayerInTown(Player, Player:GetChunkX(), Player:GetChunkZ());
 end
 
 function OnPlayerDestroyed(Player) -- This is called when a player that has been in the game disconnects
-   InTown[Player:GetName()] = nil; -- We set it to nil so Lua can garbage collect it and so the player gets the message on connection
+    InTown[Player:GetName()] = nil; -- We set it to nil so Lua can garbage collect it and so the player gets the message on connection
 end
 
 function DisplayVersion(Split, Player)
-	if not (Player == nil) then
-		Player:SendMessageInfo("Townvalds version: " .. PLUGIN:GetVersion())
-	else
-		LOG("Townvalds version: " .. PLUGIN:GetVersion())
-	end
+    if not (Player == nil) then
+        Player:SendMessageInfo("Townvalds version: " .. PLUGIN:GetVersion());
+    else
+        LOG("Townvalds version: " .. PLUGIN:GetVersion());
+    end
 
-	return true
+    return true;
 end
 
 function OnPlayerMoving(Player, OldPosition, NewPosition)
-   CheckPlayerInTown(Player, Player:GetChunkX(), Player:GetChunkZ());
-   return false;
+    CheckPlayerInTown(Player, Player:GetChunkX(), Player:GetChunkZ());
+    return false;
+end
+
+function OnPlayerBreakingBlock(Player, BlockX, BlockY, BlockZ, BlockFace, BlockType, BlockMeta)
+    return CheckBlockPermission(Player, BlockX, BlockZ);
+end
+
+function OnPlayerPlacingBlock(Player, BlockX, BlockY, BlockZ, BlockFace, BlockType, BlockMeta)
+    return CheckBlockPermission(Player, BlockX, BlockZ);
 end
 
 function CreateTable()
-	local sql = "CREATE TABLE IF NOT EXISTS users (user_guid STRING PRIMARY KEY, town_id INTEGER)";
-	result = ExecuteStatement(sql);
+    local sql = "CREATE TABLE IF NOT EXISTS users (user_guid STRING PRIMARY KEY, town_id INTEGER)";
+    result = ExecuteStatement(sql);
 
-	return true;
+    return true;
 end
--- src/main.lua
+++ src/main.lua
@@ -1,31 +1,33 @@
-PLUGIN = nil
+PLUGIN = nil;
 
 function Initialize(Plugin)
-	PLUGIN = Plugin
-	PLUGIN:SetName("Townvalds")
-	PLUGIN:SetVersion(1)
+	PLUGIN = Plugin;
+	PLUGIN:SetName("Townvalds");
+	PLUGIN:SetVersion(1);
 
-	LOG("[" .. PLUGIN:GetName() .. "] Enabling " .. PLUGIN:GetName() .. " v" .. PLUGIN:GetVersion())
+	LOG("Initialized " .. PLUGIN:GetName() .. " v." .. PLUGIN:GetVersion());
 	-- Load the Info shared library:
-	dofile(cPluginManager:GetPluginsPath() .. "/InfoReg.lua")
+	dofile(cPluginManager:GetPluginsPath() .. "/InfoReg.lua");
 
 	--Bind all the commands:
-	RegisterPluginInfoCommands()
-	RegisterPluginInfoConsoleCommands()
+	RegisterPluginInfoCommands();
+	RegisterPluginInfoConsoleCommands();
 
-	cPluginManager.AddHook(cPluginManager.HOOK_PLAYER_JOINED, OnPlayerJoined)
-	cPluginManager.AddHook(cPluginManager.HOOK_PLAYER_MOVING, OnPlayerMoving)
-	cPluginManager.AddHook(cPluginManager.HOOK_PLAYER_DESTROYED, OnPlayerDestroyed)
-        cPluginManager.AddHook(cPluginManager.HOOK_PLAYER_SPAWNED, OnPlayerSpawned)
+	cPluginManager.AddHook(cPluginManager.HOOK_PLAYER_JOINED, OnPlayerJoined);
+	cPluginManager.AddHook(cPluginManager.HOOK_PLAYER_MOVING, OnPlayerMoving);
+	cPluginManager.AddHook(cPluginManager.HOOK_PLAYER_DESTROYED, OnPlayerDestroyed);
+	cPluginManager.AddHook(cPluginManager.HOOK_PLAYER_SPAWNED, OnPlayerSpawned);
+	cPluginManager.AddHook(cPluginManager.HOOK_PLAYER_BREAKING_BLOCK, OnPlayerBreakingBlock);
+	cPluginManager.AddHook(cPluginManager.HOOK_PLAYER_PLACING_BLOCK, OnPlayerPlacingBlock);
 
 	if not (cFile:IsFile(PLUGIN:GetLocalFolder() .. "/database.sqlite3")) then -- If true, means database is deleted, or the plugin runs for the first time
-		LOG("[" .. PLUGIN:GetName() .. "] It looks like this is the first time running this plugin. Creating database...")
-		CreateDatabase()
+		LOG("[" .. PLUGIN:GetName() .. "] It looks like this is the first time running this plugin. Creating database...");
+		CreateDatabase();
 	end
 
-	return true
+	return true;
 end
 
 function OnDisable() -- Gets called when the plugin is unloaded, mostly when shutting down the server
-	LOG("[" .. PLUGIN:GetName() .. "] Disabling " .. PLUGIN:GetName() .. " v" .. PLUGIN:GetVersion())
+	LOG("Disabled " .. PLUGIN:GetName() .. "!");
 end
