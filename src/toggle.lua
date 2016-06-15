caee5c1e5477044112d60300cb5bc9c273091649
PureTryOut <PureTryOut@users.noreply.github.com>
2016-06-15 23:09:25 +0200

    Removed double OnTakeDamage function (#43)

--- src/toggle.lua
+++ src/toggle.lua
@@ -6,7 +6,7 @@ function OnExploding(World, ExplosionSize, CanCauseFire, X, Y, Z, Source, Source
 		local explosion_sql = "SELECT town_explosions_enabled FROM towns WHERE town_id = ?";
 		local explosion_parameters = {town_id};
 		if ExecuteStatement(explosion_sql, explosion_parameters)[1][1] == 0 then
-			return true; 
+			return true;
 		end
 	end
 end
@@ -17,25 +17,8 @@ function OnTakeDamage(Receiver, TDI)
         receiver_result = 1
         local town_sql = "SELECT town_id FROM townChunks WHERE chunkX = ? AND chunkZ = ?";
         local pvp_sql = "SELECT town_pvp_enabled FROM towns WHERE town_id = ?";
-        
-        local attacker_parameters = {TDI.Attacker:GetChunkX(), TDI.Attacker:GetChunkZ()}; 
-        local receiver_parameters = {Receiver:GetChunkX(), Receiver:GetChunkZ()};
 
-        if ExecuteStatement(town_sql, attacker_parameters)[1] ~= nil then
-            local attacker_town_id = ExecuteStatement(town_sql, attacker_parameters)[1][1];
-            attacker_result = ExecuteStatement(pvp_sql, {attacker_town_id})[1][1];
-        end
-    end
-end
-
-function OnTakeDamage(Receiver, TDI)
-    if Receiver:IsPlayer() and TDI.Attacker ~= nil and TDI.Attacker:IsPlayer() then
-        attacker_result = 1
-        receiver_result = 1
-        local town_sql = "SELECT town_id FROM townChunks WHERE chunkX = ? AND chunkZ = ?";
-        local pvp_sql = "SELECT town_pvp_enabled FROM towns WHERE town_id = ?";
-        
-        local attacker_parameters = {TDI.Attacker:GetChunkX(), TDI.Attacker:GetChunkZ()}; 
+        local attacker_parameters = {TDI.Attacker:GetChunkX(), TDI.Attacker:GetChunkZ()};
         local receiver_parameters = {Receiver:GetChunkX(), Receiver:GetChunkZ()};
 
         if ExecuteStatement(town_sql, attacker_parameters)[1] ~= nil then
@@ -47,7 +30,7 @@ function OnTakeDamage(Receiver, TDI)
             local receiver_town_id = ExecuteStatement(town_sql, receiver_parameters)[1][1];
             receiver_result = ExecuteStatement(pvp_sql, {receiver_town_id})[1][1];
         end
-        
+
         if attacker_result == 0 or receiver_result == 0 then
             return true;
         end
