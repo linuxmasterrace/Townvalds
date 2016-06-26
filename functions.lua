d9f4347f7df45167e9b35f6dfa2db9370b16f8dc
random-nick <random-nick@email.com>
2016-06-26 11:40:48 +0200

    Fix last online and add first joined for residents

--- functions.lua
+++ functions.lua
@@ -76,10 +76,10 @@ function GetTimestampFromString(timestring) --Returns the Lua timestamp from a s
 	return convertedTimestamp;
 end
 
-function OnPlayerJoined(Player)
+function OnPlayerJoined(Player) -- This is called after connection
 	local UUID = Player:GetUUID();
-    local sql = "INSERT OR IGNORE INTO residents (player_uuid, player_name, town_id, town_rank, last_online) VALUES (?, ?, NULL, NULL, datetime(\"now\"))";
-    local parameters = {UUID, Player:GetName()};
+    local sql = "INSERT OR IGNORE INTO residents (player_uuid, player_name, town_id, town_rank, first_joined) VALUES (?, ?, NULL, NULL, datetime(\"now\")); UPDATE residents SET last_online = NULL WHERE player_uuid = ?;";
+    local parameters = {UUID, Player:GetName(), UUID};
     if(ExecuteStatement(sql, parameters) == nil) then
         LOG("Couldn't add player "..Player:GetName().." to the database!!!");
     end
@@ -94,7 +94,11 @@ function OnPlayerSpawned(Player) -- This is called after both connection and res
 end
 
 function OnPlayerDestroyed(Player) -- This is called when a player that has been in the game disconnects
-    InTown[Player:GetUUID()] = nil; -- We set it to nil so Lua can garbage collect it and so the player gets the message on connection
+   InTown[Player:GetUUID()] = nil; -- We set it to nil so Lua can garbage collect it and so the player gets the message on connection
+   local sql = "UPDATE residents SET last_online = datetime(\"now\") WHERE player_uuid = ?";
+   local parameters = {Player:GetUUID()};
+   ExecuteStatement(sql, parameters);
+   return false;
 end
 
 function DisplayVersion(Split, Player)
