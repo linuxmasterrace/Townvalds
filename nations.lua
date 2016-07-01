4db3b9063e17eb4694c5eca392bd3e795b7f567a
PureTryOut <PureTryOut@users.noreply.github.com>
2016-07-01 12:16:57 +0200

    Fixed a typo

--- nations.lua
+++ nations.lua
@@ -99,7 +99,7 @@ function NationLeave(Split, Player)
 		local townInNationCount = ExecuteStatement(sql, parameter)[1][1];
 
 		if (nation[3] == town[1]) and (townInNationCount > 1) then
-			LeavingNation[UUID] = nil; --Make sure to clear the leaving que, otherwise the town could leave if it's set as capital between the 2 commands
+			LeavingNation[UUID] = nil; --Make sure to clear the leaving queue, otherwise the town could leave if it's set as capital between the 2 commands
 			Player:SendMessageFailure("Since your town is the capital, you can't leave the nation");
 			Player:SendMessageFailure("First set a new capital using '/town set capital [townname]'");
 		else
